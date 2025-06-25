4832995aeb82e658588c970e0dee03f501764985
JI4JUN <ji4jun0097@gmail.com>
2025-06-25 18:10:02 +0800

    feat: add 302.AI provider

--- app/client/platforms/ai302.ts
+++ app/client/platforms/ai302.ts
@@ -1,6 +1,11 @@
 "use client";
 
-import { ApiPath, AI302_BASE_URL, DEFAULT_MODELS, AI302 } from "@/app/constant";
+import {
+  ApiPath,
+  AI302_BASE_URL,
+  DEFAULT_MODELS,
+  AI302,
+} from "@/app/constant";
 import {
   useAccessStore,
   useAppConfig,
@@ -56,7 +61,10 @@ export class Ai302Api implements LLMApi {
     if (baseUrl.endsWith("/")) {
       baseUrl = baseUrl.slice(0, baseUrl.length - 1);
     }
-    if (!baseUrl.startsWith("http") && !baseUrl.startsWith(ApiPath["302.AI"])) {
+    if (
+      !baseUrl.startsWith("http") &&
+      !baseUrl.startsWith(ApiPath["302.AI"])
+    ) {
       baseUrl = "https://" + baseUrl;
     }
 
